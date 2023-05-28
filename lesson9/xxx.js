b8eac221e24d7a8c4753a3a99119671c156d1471
rastishka <kiber090477@gmail.com>
2023-05-28 12:57:55 +0300

    Initial commit

---
+++ lesson9/xxx.js
@@ -0,0 +1,10 @@
+function btnClick1() {
+   let input = document.getElementById('input1');
+   input.value = 'Button click';
+}
+
+function btnClick2() {
+   let input = document.getElementById('input2');
+   alert(input.value);
+}
+
